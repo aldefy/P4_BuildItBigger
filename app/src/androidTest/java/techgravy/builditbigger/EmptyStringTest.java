78be715821cbaab706bb774ee731414b7ecbb513
Adit Lal <aditlal90@gmail.com>
2016-02-15 15:33:58 +0530

    Step 4: Add Functional Tests

---
+++ app/src/androidTest/java/techgravy/builditbigger/EmptyStringTest.java
@@ -0,0 +1,33 @@
+package techgravy.builditbigger;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+/**
+ * Created by aditlal on 15/02/16.
+ */
+public class EmptyStringTest extends AndroidTestCase {
+
+    private static final String LOG_TAG = "EmptyStringTest";
+
+
+    @SuppressWarnings("unchecked")
+    public void runTest() {
+
+
+        // Testing that Async task successfully retrieves a non-empty string
+        // You can test this from androidTest -> Run 'All Tests'
+        String result = null;
+        EndPointsAsyncTask endpointsAsyncTask = new EndPointsAsyncTask(getContext());
+        endpointsAsyncTask.execute();
+        try {
+            result = endpointsAsyncTask.get();
+            Log.d(LOG_TAG, "Yay, Retrieved a non-empty string successfully: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(result);
+    }
+
+
+}
