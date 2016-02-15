b93c872bf21a50036ea04fa88ca09b7da039ae7a
Adit Lal <aditlal90@gmail.com>
2016-02-15 13:17:46 +0530

    Step 2:  Create an Android Library

--- androidlibrary/src/main/java/techgravy/androidlibrary/JokeDisplayActivity.java
+++ androidlibrary/src/main/java/techgravy/androidlibrary/JokeDisplayActivity.java
@@ -1,7 +1,24 @@
 package techgravy.androidlibrary;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by aditlal on 15/02/16.
  */
-public class JokeDisplayActivity {
+public class JokeDisplayActivity extends AppCompatActivity {
+    public final static String INTENTTAG = "JOKE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+        // Processing the joke intent
+        String joke = getIntent().getStringExtra(INTENTTAG);
+        TextView textViewJoke = (TextView) findViewById(R.id.textview_joke);
+        textViewJoke.setText(joke);
+    }
+
+
 }
