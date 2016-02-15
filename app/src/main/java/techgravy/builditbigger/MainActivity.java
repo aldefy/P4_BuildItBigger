57653ebdddf090bb2381acdff42dd1b0e98d81f6
Adit Lal <aditlal90@gmail.com>
2016-02-15 11:29:54 +0530

    Ad view configured

--- app/src/main/java/techgravy/builditbigger/MainActivity.java
+++ app/src/main/java/techgravy/builditbigger/MainActivity.java
@@ -1,13 +1,46 @@
 package techgravy.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.adView)
+    AdView adView;
+    @Bind(R.id.instructionsTextView)
+    TextView instructionsTextView;
+    @Bind(R.id.tellJokeButton)
+    Button tellJokeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setupAd();
+    }
+
+    private void setupAd() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adView.loadAd(adRequest);
+
+    }
+
+    @OnClick(R.id.tellJokeButton)
+    public void onClick() {
+        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
     }
 }
